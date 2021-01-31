81e94e1618ed14cfa45d0ab47a963edfd434a96a
Rafael Japyassu <rafa.japyassu@gmail.com>
2021-01-31 12:12:17 -0300

    Adicionando Cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import './config/env';
 import 'express-async-errors';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import './database';
@@ -10,6 +11,7 @@ import AppError from './errors/AppError';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
